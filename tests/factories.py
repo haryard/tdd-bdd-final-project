28af8425c2ad0aea33fa9cdfc7f50bd8f9b5a493
Hary Ardika <harry.vanaheim@gmail.com>
2024-09-25 06:04:04 -0400

    Add some test case in tests/test_models.py

--- tests/factories.py
+++ tests/factories.py
@@ -31,7 +31,7 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-    ## Add code to create Fake Products
+    # Add code to create Fake Products
     name = FuzzyChoice(
         choices=[
             "Hat",
@@ -47,7 +47,7 @@ class ProductFactory(factory.Factory):
             "Wrench"
         ]
     )
-    description = factory.faker('text')
+    description = factory.Faker('text')
     price = FuzzyDecimal(0.5, 2000.0, 2)
     available = FuzzyChoice(choices=[True, False])
     category = FuzzyChoice(
